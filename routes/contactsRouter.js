81a72582b5d0f71ede06af9563ed3b4ba68dc46e
VictorFil67 <victorfilinovych@gmail.com>
2024-06-26 22:56:42 +0300

    first commit

---
+++ routes/contactsRouter.js
@@ -0,0 +1,46 @@
+import express from "express";
+import contactsControllers from "../controllers/contactsControllers.js";
+import validateBody from "../decorators/validateBody.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+import isValidId from "../middlewares/isValidId.js";
+import authtenticate from "../middlewares/authenticate.js";
+import upload from "../middlewares/upload.js";
+
+const contactsRouter = express.Router();
+
+contactsRouter.use(authtenticate);
+
+const {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+  updateStatusContact,
+} = contactsControllers;
+contactsRouter.get("/", getAllContacts);
+
+contactsRouter.get("/:id", isValidId, getOneContact);
+
+contactsRouter.delete("/:id", isValidId, deleteContact);
+
+contactsRouter.post(
+  "/",
+  upload.single("avatarURL"),
+  validateBody(createContactSchema),
+  createContact
+);
+
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(updateContactSchema),
+  updateContact
+);
+
+contactsRouter.patch("/:id/favorite", isValidId, updateStatusContact);
+
+export default contactsRouter;
